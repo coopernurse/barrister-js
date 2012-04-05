9754b227e789df8ab9fa74c42877c898a0972fe0
James Cooper <james@bitmechanic.com>
2012-04-05 12:48:22 -0700

    added docs on overriding the ajax POST to avoid need for jquery

--- src/browser_footer.js
+++ src/browser_footer.js
@@ -1,7 +1,23 @@
 
+// Constructs a new httpClient
+// 
+// o may be a string (URL endpoint to access) or a function
+// 
+// If o is a function:
+//  * When a request is made, your function will get two args: request, callback
+//  * request is an object that represents JSON-RPC request
+//  * your code should call: Barrister.JSON_stringify(req) to serialize it to JSON. This stringify
+//    function properly escapes non-ASCII characters to ensure uniform behavior across servers.
+//  * Make a POST request to the endpoint as desired, make sure to set Content-Type to
+//    "application/json"
+//  * Invoke the callback with a JSON-RPC response.  Use the `Barrister.parseHttpResponse` helper
+//    function to simplify this.  Pass it the original req you were given, 
+//    an error string (if the request failed, or null if succeeded), and the raw response body.
+//
 var httpClient = function(o) {
     var transport = o;
     if (typeof o === "string") {
+        // o is 
         transport = function(req, callback) {
             var settings = {
                 type: "POST",
@@ -22,6 +38,7 @@ var httpClient = function(o) {
 };
 
 Barrister.httpClient = httpClient;
+Barrister.parseHttpResponse = parseHttpResponse;
 Barrister.Client = Client;
 Barrister.Contract = Contract;
 Barrister.JSON_stringify = JSON_stringify;
