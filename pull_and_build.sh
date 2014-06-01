647c5b2659c700ed057c873942626e31ed590408
James Cooper <jamespcooper@gmail.com>
2014-05-31 18:46:43 -0700

    fix activate path

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-BARRISTER=/usr/local/github/barrister
-source $BARRISTER/env/activate
+export BARRISTER=/usr/local/github/barrister
+source $BARRISTER/env/bin/activate
 export PYTHONPATH=$PYTHONPATH:$BARRISTER
 export PATH=$PATH:$BARRISTER/bin
 
 git pull
 npm install
+echo $PATH
 make clean all
 #rsync -avz docs/ james@barrister.bitmechanic.com:/home/james/barrister-site/api/js/latest/
 #rsync -avz post-build/ james@barrister.bitmechanic.com:/home/james/barrister-site/dist/js/latest/
